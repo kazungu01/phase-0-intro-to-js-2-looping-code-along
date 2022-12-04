4d610c93001707865ef50ec89c08f34fc11e2771
hassan kazungu <hassankazungu01@gmail.com>
2022-12-04 12:51:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+function writeCards([khalid, joy, ivanca], birthday) {
+    return (writeCards = [
+      `Thank you, ${khalid}, for the wonderful ${birthday} gift!`,
+      `Thank you, ${joy}, for the wonderful ${birthday} gift!`,
+      `Thank you, ${ivanca}, for the wonderful ${birthday} gift!`,
+    ]);
+    debugger;
+  }
+  const num = 10;
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+    return num;
+  }
